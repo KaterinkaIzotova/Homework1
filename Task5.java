50913e8dc39ffb133bbeb4cef3b5c1faff59f412
KaterinkaIzotova <147706130+KaterinkaIzotova@users.noreply.github.com>
2023-10-12 23:08:33 +0300

    Изменила 5 и 7

--- Task5.java
+++ Task5.java
@@ -2,7 +2,6 @@
  * Создать двумерный  3х3 массив, заполнить его через scanner,
  * вывести на консоль введенный массив и удвоенный массив
  */
-import java.util.Arrays;
 import java.util.Scanner;
 public class Task5 {
     public static void main (String[] args) {
@@ -16,12 +15,19 @@ public class Task5 {
                 twoArray[i][j] = scan.nextInt();
             }
         }
-
+        String str = "";
+        String str2 = "";
         for (int i=0; i<3; i++) {
+            String tmp = "";
+            String tmp2 = "";
             for (int j=0; j<3; j++) {
-
-                System.out.print(twoArray[i][j]);
+                tmp = tmp + " " + twoArray[i][j];
+                tmp2 = tmp2 + " " + twoArray[i][j]*2;
             }
+            str = str + "\n" + tmp;
+            str2 = str2 + "\n" + tmp2;
         }
+        System.out.println(str);
+        System.out.println(str2);
     }
 }
--- Task7.java
+++ Task7.java
@@ -6,12 +6,11 @@ public class Task7 {
     public static void main (String[] args) {
         int counter = 1;
         do {
-            if (counter%5 ==0) {
+            if (counter % 5 == 0) {
                 System.out.println(counter);
-                counter ++;}
-            else {counter++;
             }
-        } while (counter <=100);
-
+            counter++;
+        }
+            while (counter <= 100) ;
     }
 }
